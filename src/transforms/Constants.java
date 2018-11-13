32362c0033e7f4b253991e928f08c310f3f34f41
Mickael <mickael.spilmont.fr@gmail.com>
2018-11-13 11:36:10 +0100

    Suppression de l'IHM, modification des constantes (200 -> 0), ajout de comentaire et todo

--- src/transforms/Constants.java
+++ src/transforms/Constants.java
@@ -3,12 +3,12 @@ package transforms;
 import javafx.geometry.Point2D;
 
 public class Constants {
-    static final double ZERO_X = 200.0; // Abcisse de l'origine
+    static final double ZERO_X = 0.0; // Abcisse de l'origine
     static final double MIN_X = 0.0;    // Bord gauche
     static final double MAX_X = 400.0;  // Bord Droit
     static final double INC_X = 30.0;   // Pas du quadrillage
 
-    static final double ZERO_Y = 200.0; // Ordonnée de l'origine
+    static final double ZERO_Y = 0.0; // Ordonnée de l'origine
     static final double MIN_Y = 0.0;    // Bord haut
     static final double MAX_Y = 400.0;  // Bord bas
     static final double INC_Y = 30.0;   // Pas du quadrillage
